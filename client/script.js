5d50100147040a363a7e81644b5755eff556b293
Vinston Guillaume <vinstonguillaume@gmail.com>
2018-12-10 23:33:49 -0500

    mp3 compatible

--- client/script.js
+++ client/script.js
@@ -1,11 +1,20 @@
-var convertBtn = document.querySelector('.convert-button');
+var mp4 = document.querySelector('.mp4');
+var mp3 = document.querySelector('.mp3');
 var URLinput = document.querySelector('.URL-input');
 
-convertBtn.addEventListener('click', () => {
+mp4.addEventListener('click', () => {
 	console.log(`URL: ${URLinput.value}`);	
-	sendURL(URLinput.value);
+	sendMP4(URLinput.value);
+});
+mp3.addEventListener('click', () => {
+	console.log(`URL: ${URLinput.value}`);	
+	sendMP3(URLinput.value);
 });
 
-function sendURL(URL) {
-	window.location.href = `http://35.237.46.180:4000/download?URL=${URL}`;
+function sendMP4(URL) {
+	window.location.href = `http://35.237.46.180:4000/mp4?URL=${URL}`;
+}
+
+function sendMP3(URL) {
+	window.location.href = `http://35.237.46.180:4000/mp3?URL=${URL}`;
 }
--- server/app.js
+++ server/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
-
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+const ffmpeg = require('fluent-ffmpeg');
+var horizon = require('horizon-youtube-mp3');
+var http = require('http');
+var url  = require('url') ;
+var log = require('console-log-level')({ level: 'info' });
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
 const app = express();
 
 app.use(cors());
@@ -10,10 +16,34 @@ app.listen(4000, () => {
 	console.log('Server Works !!! At port 4000');
 });
 
-app.get('/download', (req,res) => {
+app.get('/mp4', (req,res) => {
 	var URL = req.query.URL;
 	res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 	ytdl(URL, {
 		format: 'mp4'
 	}).pipe(res);
 });
+
+
+
+app.get('/mp3', (req, res) => {
+	
+	var paramsUrl = req.query.URL;
+  log.info('URL Video: ' + paramsUrl);
+
+  var cropParams = null;
+
+  horizon.download(paramsUrl, res, null, cropParams, null, false, function(err, e){
+
+    if(err) {
+      return log.info(err);
+    }
+
+    if(e === horizon.successType.CONVERSION_FILE_COMPLETE){
+	  log.info(e);
+	  
+    }
+
+});
+	})
+
