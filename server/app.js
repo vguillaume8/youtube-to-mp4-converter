fd8a72bfc8d3116b016b676601bcc430f25857b0
Vinston Guillaume <vinstonguillaume@gmail.com>
2019-01-04 03:02:12 -0500

    added email support

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const nodemailer = require('nodemailer');
 const path = require('path');
 const soundcloudDl = require("soundcloud-dl");
 const cors = require('cors');
@@ -14,9 +15,11 @@ mongoose.connect(`mongodb+srv://${DBCONFIG}@cluster0-zb5te.gcp.mongodb.net/media
 const connection = mongoose.connection;
 var log = require('console-log-level')({ level: 'info' });
 ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+let email = 'turbostore2019@gmail.com'
 const app = express();
 
-console.log(DBCONFIG);
+
+
 app.use(cors());
 
 app.listen(4000, () => {
@@ -26,25 +29,31 @@ app.listen(4000, () => {
 connection.on('connected', function(){
 	console.log("DB connected");
   })
+ let split= DBCONFIG.split(':');
+ let pass = split[1];
 
-function parseData(URL, IP){
-	return user = {
-		date: new Date(),
-		ip: IP,
-		url: URL
-	};
-}
+
+  var transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+	  user: 'vmoney507@gmail.com' ,
+	  pass: pass
+	}
+  });
+
+
 
 app.get('/mp4', (req,res) => {
 	var URL = req.query.URL;
-	var user = parseData(URL, req.query.IP);
+	var IP = req.query.IP;
+	var user = parseData(URL, IP);
 	var data = new DataModel(user);
 	data.save(function(err, user){
 		if(err){
 			console.log("Could not save to database");
 		}
 	});
-	
+	sendMail(URL, IP);
 	res.header('Content-Disposition', 'attachment; filename="video.mp4"');
 	ytdl(URL, {
 		format: 'mp4'
@@ -55,6 +64,7 @@ app.get('/mp4', (req,res) => {
 app.get('/mp3', (req, res) => {
 	var user = parseData(req.query.URL, req.query.IP)
 	var data = new DataModel(user);
+	var IP = req.query.IP;
 	data.save(function(err, user){
 		if(err){
 			console.log("Could not save to database");
@@ -62,6 +72,7 @@ app.get('/mp3', (req, res) => {
 	});
 	
   var paramsUrl = req.query.URL;
+  sendMail(paramsUrl, IP);
   log.info('URL Video: ' + paramsUrl);
 
   var cropParams = null;
@@ -82,18 +93,46 @@ app.get('/mp3', (req, res) => {
 });
 
 app.get('/soundcloud', (req, res, next) => {
-	var user = parseData(req.query.URL, req.query.IP)
+	var IP = req.query.IP;
+	var URL = req.query.URL;
+	var user = parseData(URL, IP)
 	var data = new DataModel(user);
 	data.save(function(err, user){
 		if(err){
 			console.log("Could not save to database");
 		}
 	});
-
-	var url = req.query.URL;
-	soundcloudDl.getSongDlByURL(url).then(function(song){  
+	sendMail(URL, IP);
+	soundcloudDl.getSongDlByURL(URL).then(function(song){  
 	  res.redirect(song.http_mp3_128_url);
 });
 	
 });
 
+
+function sendMail(URL, IP){
+	var mailOptions = {
+		from: email,
+		to: 'vinstonguillaume@gmail.com',
+		subject: 'New Download From Media Converter!',
+		html: `<h1>${new Date()}</h1><p>IP Adress: ${IP}</p><p>URL: ${URL}</p>`
+	  };
+
+	transporter.sendMail(mailOptions, function(error, info){
+	if (error) {
+		console.log(error);
+	} else {
+		console.log('Email sent: ' + info.response);
+	}
+	});
+}
+
+
+
+function parseData(URL, IP){
+	return user = {
+		date: new Date(),
+		ip: IP,
+		url: URL
+	};
+}
